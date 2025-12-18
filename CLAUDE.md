3c41ba65fde7a610086c7382227239baf1dfce19
leo.yi <leo.yi@kakaocorp.com>
2025-12-19 00:59:10 +0900

    html, body, main migration

--- CLAUDE.md
+++ CLAUDE.md
@@ -3,6 +3,11 @@
 마이그레이션하면서 한 의사결정은 모두 이 파일에 기록.
 - store 마이그레이션 완료. 현재 store 구조를 존중
 
+기본적으로 아래 순서로 마이그레이션. 
+- 요청된 기능에 연관된 기존 코드 탐색
+- 기존 코드를 현재 프로젝트의 적합한 위치에 복사
+- 현재 프로젝트의 컨벤션과 react-router에 맞게 기존 코드를 수정 
+
 Use react 19.
 Use react router v7.
 Prefer shadcn component. Install if required.
--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,38 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import {
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLocation,
+} from "react-router";
 
 import "./app.css";
+import type { Route } from ".react-router/types/app/+types/root";
+import clsx from "clsx";
 
-export default function App() {
+export default function App({ params }: Route.ComponentProps) {
+	const location = useLocation();
+	const isMain = location.pathname === "/";
+	const paddingLeft = isMain ? `sm:pl-[11rem]` : "sm:pl-[6.25rem]";
+
 	return (
-		<html lang="ko">
+		<html
+			lang={params.locale}
+			className="bg-neutral-900 font-normal text-neutral-950"
+		>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
 				<Links />
 			</head>
-			<body>
-				<Outlet />
+			<body className="sm:min-w-[1200px]">
+				<main
+					className={clsx("flex min-h-full min-w-full flex-col", paddingLeft)}
+				>
+					<Outlet />
+				</main>
 				<ScrollRestoration />
 				<Scripts />
 			</body>
